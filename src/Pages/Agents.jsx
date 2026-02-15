bf1a6387d2b2a038d5487a2cb1eebfcb573ae7e9
Swagat Group <swagatgroupinfo@gmail.com>
2026-02-15 15:52:07 +0530

    first commit

---
+++ src/Pages/Agents.jsx
@@ -0,0 +1,81 @@
+
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+import React, { useRef } from 'react'
+
+const Agents = () => {
+
+  gsap.registerPlugin(ScrollTrigger)
+
+  const imageDivRef = useRef(null)
+  const imageRef = useRef(null)
+
+const imageArray = ['../src/assets/Agents-img/a1.jpg', 
+                      '../src/assets/Agents-img/a2.jpg', 
+                      '../src/assets/Agents-img/a3.jpg', 
+                      '../src/assets/Agents-img/a4.jpg', 
+                      '../src/assets/Agents-img/a5.jpg', 
+                      '../src/assets/Agents-img/a6.jpg', 
+                      '../src/assets/Agents-img/a7.jpg', 
+                      '../src/assets/Agents-img/a1.jpg', 
+                      '../src/assets/Agents-img/a2.jpg', 
+                      '../src/assets/Agents-img/a3.jpg', 
+                      '../src/assets/Agents-img/a4.jpg', 
+                      '../src/assets/Agents-img/a5.jpg', 
+                      '../src/assets/Agents-img/a6.jpg', 
+                      '../src/assets/Agents-img/a7.jpg']
+
+  useGSAP(function () {
+
+    gsap.to(imageDivRef.current, {
+      scrollTrigger: {
+        trigger: imageDivRef.current,
+        // markers: true,
+        start: 'top 28%',
+        end: 'top -70%',
+        pin: true,
+        pinSpacing: true,
+        pinReparent: true,
+        pinType: 'transform',
+        scrub: 1, // smooth scrubbing with 1s easing
+        anticipatePin: 1,
+        invalidateOnRefresh: true,
+        onUpdate: (elem) => {
+          let imageIndex;
+          if (elem.progress < 1) {
+            imageIndex = Math.floor(elem.progress * imageArray.length)
+          } else {
+            imageIndex = imageArray.length - 1
+          }
+          imageRef.current.src = imageArray[imageIndex]
+        }
+      }
+    })
+  })
+
+
+  return (
+    <div className='parent'>
+      <div id='page1' className='py-1'>
+        <div ref={imageDivRef} className='absolute overflow-hidden h-[20vw] rounded-3xl w-[15vw] top-64 left-[30vw]'>
+          <img ref={imageRef} className='h-full object-cover w-full' src="https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg" alt="" />
+        </div>
+        <div className='relative font-[LAUSANNE-500]'>
+          <div className='lg:mt-[55vh] mt-[30vh]'>
+            <h1 className='text-[20vw] text-center uppercase leading-[18vw]'>Soixan7e <br />
+              Douze</h1>
+          </div>
+          <div className='lg:pl-[40%] lg:mt-20 mt-4 p-3'>
+            <p className='lg:text-5xl text-xl leading-tight'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Notre curiosité nourrit notre créativité. On reste humbles et on dit non aux gros egos, même le vôtre. Une marque est vivante. Elle a des valeurs, une personnalité, une histoire. Si on oublie ça, on peut faire de bons chiffres à court terme, mais on la tue à long terme. C’est pour ça qu’on s’engage à donner de la perspective, pour bâtir des marques influentes.</p>
+          </div>
+        </div>
+      </div>
+      <div id='page2' className=" h-screen">
+
+      </div>
+    </div>
+  )
+}
+
+export default Agents
